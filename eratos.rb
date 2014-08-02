d06152276331166bfc6c8b1d7e6cb08f5022802a
Shunichi Ikegami <sike.tm@gmail.com>
2014-08-02 15:25:51 +0900

    Add implementation.

--- eratos.rb
+++ eratos.rb
@@ -1,9 +1,18 @@
 module Eratos
   def self.primes(n)
-    []
+    return [] if n < 2
+
+    limit = Math.sqrt(n).floor
+    numbers = (2..n).to_a
+    results = []
+    while (p = numbers[0]) <= limit
+      results.push p
+      numbers -= (1..(n/p)).map{ |m| m * p }
+    end
+    results + numbers
   end
 end
 
 if __FILE__ == $0
-  puts Eratos.primes(ARGV.shift).join(', ')
+  puts Eratos.primes(ARGV.shift.to_i).join(', ')
 end
